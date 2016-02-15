a5bb30f67ff776d1b35d770b5c48b7bb4d42b3f6
Hack Reactor Students <accounts@hackreactor.com>
2016-02-15 12:26:20 -0800

    Initialize Nodes and People collections
    Also add methods for addNode, removeNode, and addConnection

---
+++ collections.js
@@ -0,0 +1,17 @@
+Nodes = new Mongo.Collection("Nodes");
+
+// personId is a number
+// partnerId is a number
+// parternerIds is an array of numbers
+
+Meteor.methods({
+  "addNode": function (personId, partnerIds) {
+    return Nodes.insert({id: personId, partnerIds: partnerIds});
+  },
+  "removeNode": function (personId) {
+    Nodes.remove({id: personId});
+  },
+  "addConnection": function (personId, partnerId) {
+    Nodes.update({id: personId}, {$addToSet: {partnerIds: partnerId}});
+  }
+});
