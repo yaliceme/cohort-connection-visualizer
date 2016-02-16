ac4ad0262c1d49ababfa68a9d0ca0482f828b482
Hack Reactor Students <accounts@hackreactor.com>
2016-02-15 16:26:24 -0800

    Flesh out App render function

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,32 @@
 App = React.createClass({
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      nodes: Nodes.find({}).fetch()
+    }
+  },
+
   render: function () {
-    return <MeSelector />
+    return (
+      <div>
+        <div className="page-header">
+          <center>
+            <h1>
+            <i className="fa fa-beer"></i> Connection <small> Visualizer</small>
+            </h1>
+          </center>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <MeSelector />
+          </div>
+          <div className="col-md-offset-2 col-md-6">
+          </div>
+        </div>
+      </div>
+      </div>
+    );
   }
 });
